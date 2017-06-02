ed35a0cd18387fe9507a9c2018190ac77edb6d60
Raghavendra <lade.raghavendraece@gmail.com>
2017-06-03 00:50:37 +0530

    phtos rotation done

--- app/scripts/wishes.js
+++ app/scripts/wishes.js
@@ -14,23 +14,25 @@
           return newArray;
         }
     })
-      .controller("flipPhoto",['$scope','shuffleItems','$timeout', function($scope,shuffleItems,$timeout){
+      .controller("flipPhoto",['$scope','shuffleItems','$timeout','$interval', function($scope,shuffleItems,$timeout,$interval){
           var initialArr = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg','15.jpg','16.jpg','17.jpg','18.jpg','19.jpg','20.jpg'];
           $scope.imagesArr = shuffleItems.shuffle(initialArr)
-          console.log($scope.imagesArr)
-          for(var iCtrl = 0;iCtrl< $scope.imagesArr.length;iCtrl++){  
-            (function(iCtrl){  // i will now become available for the someMethod to call
-               $( "#picture" ).removeClass( "flip" );
-               $timeout(function() {
-                    imageDisplay(iCtrl);
-                }, iCtrl * 5000);
-               
-            })(iCtrl); 
-            $( "#picture" ).removeClass( "flip" );
-          }
-           function imageDisplay(iCtrl){
-                $( "#picture" ).addClass( "flip" );
-                $scope.imageDisplayed = $scope.imagesArr[iCtrl];   
-           }
-      }]);
+          $scope.displayPicFrame = true;
+            var iCtrl = 0;
+            $scope.imageDisplayed = $scope.imagesArr[iCtrl];
+           var intervalTime =  $interval(function(){
+                iCtrl++;
+                if(iCtrl%2 == 0){
+                  $timeout(function() {
+                  $scope.imageDisplayed = $scope.imagesArr[iCtrl];
+                  }, 3000);
+                }else{
+                  $scope.imageDisplayed = $scope.imagesArr[iCtrl];
+                }
+                  if(iCtrl == initialArr.length){
+                    clearInterval(intervalTime);
+                    $scope.displayPicFrame = false;
+                  }
+          },8000)
+       }]);
   })();
