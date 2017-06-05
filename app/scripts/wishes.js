85a63de531bd66b02f56bb3f993289778a7e913c
Raghavendra <lade.raghavendraece@gmail.com>
2017-06-05 07:27:07 +0530

    Latest changes in the content

--- app/scripts/wishes.js
+++ app/scripts/wishes.js
@@ -16,13 +16,32 @@
     })
       .controller("flipPhoto",['$scope','shuffleItems','$timeout','$interval', function($scope,shuffleItems,$timeout,$interval){
           var initialArr = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg','15.jpg','16.jpg','17.jpg','18.jpg','19.jpg','20.jpg'];
-          $scope.imagesArr = shuffleItems.shuffle(initialArr)
+          var namesIniArr = ['Deepika','Dimple Queen','Campus Beauty(CB)','Mouni','Strawberry','Deepu','Mani','Deeksha','Papam from Polavaram','Sotta Buggala Sundari']
+          $scope.namesArr = shuffleItems.shuffle(namesIniArr);
+          $scope.imagesArr = shuffleItems.shuffle(initialArr);
           $scope.displayPicFrame = true;
+          $scope.displayVideoFrame = false;
+         
             var iCtrl = 0;
+            var nameCtrl = 0;
+            $scope.nameDisplayed = $scope.namesArr[nameCtrl];
             $scope.imageDisplayed = $scope.imagesArr[iCtrl];
+
+            $scope.numberDisplayed = 3;
+            var readyCount = $interval(function(){
+              $scope.numberDisplayed--
+              if($scope.numberDisplayed == 1){
+                $interval.cancel(readyCount);
+                $scope.displayVideoFrame = true;
+              }
+            },8000)
+            
+
            var intervalTime =  $interval(function(){
                 iCtrl++;
-                if(iCtrl%2 == 0){
+                nameCtrl++;
+                $scope.nameDisplayed = $scope.namesArr[nameCtrl];
+                if(iCtrl%2 == 0){ 
                   $timeout(function() {
                   $scope.imageDisplayed = $scope.imagesArr[iCtrl];
                   }, 3000);
@@ -30,8 +49,9 @@
                   $scope.imageDisplayed = $scope.imagesArr[iCtrl];
                 }
                   if(iCtrl == initialArr.length){
-                    clearInterval(intervalTime);
+                    $interval.cancel(intervalTime);
                     $scope.displayPicFrame = false;
+                    $scope.displayNum = true;
                   }
           },8000)
        }]);
