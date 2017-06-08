7c72781e21e1b7f6f55e1a65ff1130ca44d4cdd6
Raghavendra <lade.raghavendraece@gmail.com>
2017-06-09 00:12:54 +0530

    Array

--- app/scripts/wishes.js
+++ app/scripts/wishes.js
@@ -17,6 +17,7 @@
       .controller("flipPhoto",['$scope','shuffleItems','$timeout','$interval', function($scope,shuffleItems,$timeout,$interval){
           var initialArr = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg','13.jpg','14.jpg','15.jpg','16.jpg','17.jpg','18.jpg','19.jpg','20.jpg'];
           var namesIniArr = ['Deepika','Dimple Queen','Campus Beauty(CB)','Mouni','Strawberry','Deepu','Mani','Deeksha','Papam from Polavaram','Sotta Buggala Sundari']
+          var videosArr = ["Ragha and Namratha","Sreelatha","Roja","Suma","Sravya and Madhu","Vinitha","Nidhi","Hema","Ramya","Sravanthi","Jenny","Mahalakshmi","Raghavendra","Namratha","Gnaneshwari","Praneeth","Nandini","Santu","Nagalatha","Charan","Soni","Amma","Nanna"]
           $scope.namesArr = shuffleItems.shuffle(namesIniArr);
           $scope.imagesArr = shuffleItems.shuffle(initialArr);
           $scope.displayPicFrame = true;
