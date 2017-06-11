337bb80b383415a51ec06e0e1b229c51a045fbcc
Raghavendra <lade.raghavendraece@gmail.com>
2017-06-11 09:28:53 +0530

    Happy bday deepu

--- app/scripts/wishes.js
+++ app/scripts/wishes.js
@@ -26,14 +26,13 @@
     .controller("flipPhoto", ['$scope', 'shuffleItems', '$timeout', '$interval', 'ajaxcallservice', function ($scope, shuffleItems, $timeout, $interval, ajaxcallservice) {
       var initialArr = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
       var namesIniArr = ['Deepika', 'Dimple Queen', 'Campus Beauty(CB)', 'Mouni', 'Strawberry', 'Deepu', 'Mani', 'Deeksha', 'Papam from Polavaram', 'Sotta Buggala Sundari']
-      $scope.quotesArr = ["Nee Chirunavvutho chinukulu saitam kurpinchagalavu, Nee Sottabuggala lo samudrani saitam nimpagalavu", "Aa deepam lo ayna nune lekunda chudagalam kani, Ee deepika moham lo navvu lekunda chudalem", "Manishi: Oy Chirunavvu nuvu ekkada vuntavu ante, Chirunavvu: Na C/O Address Manideepika face ani anade"]
-      // var videosArr = ["Sreelatha", "Roja", "Suma", "Sravya and Madhu", "Vinitha", "Nidhi", "Hema", "Ramya", "Sravanthi", "Jenny", "Mahalakshmi", "Raghavendra", "Namratha", "Gnaneshwari", "Praneeth", "Nandini", "Santu", "Latha", "Charan", "Soni", "Amma", "Nanna"]
+      $scope.quotesArr = ["Nee Chirunavvutho chinukulu saitam kurpinchagalavu...,Nee Sottabuggala lo samudrani saitam nimpagalavu", "ఆ దీపం లో అయిన నూనీ లేకుండా చూడగలం కానీ..., ఈ దీపికా మొహం లో నవ్వు లేకుండా చూడలేం", "మనిషి : ఓయ్ చిరునవ్వు నువు ఎక్కడ ఉంటావు అని అడుగుతే ?, చిరునవ్వు : నేను ఎప్పుడైనా దీపికా మొహం మీద ఉంటాను అని అన్నది","When another Birthday comes around,then say that you are one year wiser not older","ఎప్పుడు నవ్వుతూ ఉండు అప్పుడు, ఈ ప్రపంచంలో ఎవరూ నీకన్న అందంగా ఉండరు"]
+      $scope.thanksArrLeft=["Producer : Raghavendra (Laptop)","Technicians : Raghavendra,Gnaneshwari,Vamsi(Coding)","Photography: Soni, Soujanya","Cast and Crew:"," Sreelatha, Roja, Suma, Sravya, Madhu, Vinitha", "Nidhi, Hema, Ramya, Sravanthi, Soujanya, Anusha", "Sai Roja, Jenny, Mahalakshmi, Raghavendra, Namratha ", "Gnaneshwari, Praneeth,Nandini, Santu, Vamsi, Latha", "Charan, Soni, Amma, Nanna","--A film by Vamsi Ram"]
+
+
       ajaxcallservice.getWishes('app/json/videos.json').then(function (response) {
         $scope.videosArr = response; //Assign data received to $scope.employees
       });
-
-
-
       $scope.namesArr = shuffleItems.shuffle(namesIniArr);
       $scope.imagesArr = shuffleItems.shuffle(initialArr);
       $scope.displayPicFrame = true;
@@ -49,14 +48,12 @@
         }
 
       }
-
       var iCtrl = 0;
       var nameCtrl = 0;
       var quotesCtrl = -1;
       $scope.textArr = [];
       $scope.nameDisplayed = $scope.namesArr[nameCtrl];
       $scope.imageDisplayed = $scope.imagesArr[iCtrl];
-
       var intervalTime = $interval(function () {
         iCtrl++;
         nameCtrl++;
@@ -67,18 +64,12 @@
           quotesCtrl++
           $scope.displayNames = false;
           if (quotesCtrl < $scope.quotesArr.length) {
-
             var textDisplayed = $scope.quotesArr[quotesCtrl];
-            console.log($scope.quotesArr[quotesCtrl])
-            console.log(textDisplayed);
             $scope.textArr = textDisplayed.split(',');
             $scope.line1 = $scope.textArr[0],
-              $scope.line2 = $scope.textArr[1];
-            console.log($scope.textArr)
+            $scope.line2 = $scope.textArr[1];
           }
-
         }
-
         if (iCtrl % 2 == 0) {
           $timeout(function () {
             $scope.imageDisplayed = $scope.imagesArr[iCtrl];
